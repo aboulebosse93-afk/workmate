ffd58a2e676e2055512393aa3a43424c3eb49f10
aboulebosse93-afk <aboulebosse93@gmail.com>
2026-06-07 19:48:00 +0200

    Delete chrome-extension-template directory

--- chrome-extension-template/content.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessage.addListener((msg) => {
-  if (msg.type === 'WORKMATE_CAPTURE') {
-    const text = document.body.innerText.slice(0, 12000);
-    const url = 'https://workmate-gamma.vercel.app/?capture=' + encodeURIComponent(text);
-    window.open(url, '_blank');
-  }
-});
